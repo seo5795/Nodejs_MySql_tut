6c4ddffb32c163db0887ba269838afccca7e279b
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-10-27 14:36:17 +0900

    db연결

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '8810',
+  database : 'seodb'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+  if (error){
+      console.log(error);
+  }
+  console.log(results);
+});
+ 
+connection.end()
