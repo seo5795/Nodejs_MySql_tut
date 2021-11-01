d2583bb4e96106f45592ac30a20dcf9bbbe7f267
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-01 16:25:00 +0900

    mysql 글수정 오류 해결: author_id는 받지 않으므로 임의적으로 1을 준다

--- main.js
+++ main.js
@@ -102,30 +102,30 @@ var app = http.createServer(function(request,response){
         if(error){
           throw error;
         }
-        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2,topic){
-        if(error2){
-          throw error2;
-        }
-        
-        var list = template.list(topics);
-        var html = template.HTML(topic[0].title, list,
-                `
-                <form action="/update_process" method="post">
-                <input type="hidden" name="id" value="${topic[0].id}">
-                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-                <p>
-                  <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-                </p>
-                <p>
-                  <input type="submit">
-                </p>
-              </form>`,
-                `<a href="/create">create</a><a href="/update?id=${topic[0].id}">update</a>`
-                );
-            response.writeHead(200);
-            response.end(html);
+        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+          if(error2){
+            throw error2;
+          }
+          var list = template.list(topics);
+          var html = template.HTML(topic[0].title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${topic[0].id}">
+              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
       });
-    });
     } else if(pathname === '/update_process'){
       var body = '';
       request.on('data', function(data){
