adefe3d2231ebb3f00c075a5d9cc3ff6265e79a3
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-01 16:29:47 +0900

    mysql 삭제기능 구현

--- main.js
+++ main.js
@@ -134,7 +134,7 @@ var app = http.createServer(function(request,response){
       request.on('end', function(){
           var post = qs.parse(body);
           db.query(`
-            UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [post.title, post.description, post.id],
+            UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`, [post.title, post.description, post.id],
             function(error, result){
               response.writeHead(302, {Location: `/?id=${post.id}`});
             response.end();
@@ -150,7 +150,10 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var id = post.id;
           var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
+          db.query('DELETE FROM topic WHERE id = ?', [post.id], function(error,result){
+            if(error){
+              throw error;
+            }
             response.writeHead(302, {Location: `/`});
             response.end();
           })
