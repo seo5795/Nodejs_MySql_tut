4700d963b8cda0deadc7e3b52bf4ad7181b08df0
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-01 16:12:04 +0900

    update process 수정되지 않는 오류

--- main.js
+++ main.js
@@ -98,13 +98,13 @@ var app = http.createServer(function(request,response){
           )
       });
     } else if(pathname === '/update'){
-      db.query(`SELECT * FROM topic`, function(error, topics){
+      db.query('SELECT * FROM topic', function(error, topics){
         if(error){
           throw error;
         }
         db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2,topic){
         if(error2){
-          throw error2
+          throw error2;
         }
         
         var list = template.list(topics);
@@ -112,7 +112,7 @@ var app = http.createServer(function(request,response){
                 `
                 <form action="/update_process" method="post">
                 <input type="hidden" name="id" value="${topic[0].id}">
-                <p><input type="text" name="title" placeholder="${topic[0].title}"></p>
+                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
                 <p>
                   <textarea name="description" placeholder="description">${topic[0].description}</textarea>
                 </p>
@@ -133,15 +133,13 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
+          db.query(`
+            UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [post.title, post.description, post.id],
+            function(error, result){
+              response.writeHead(302, {Location: `/?id=${post.id}`});
+            response.end();
+            }
+          )
       });
     } else if(pathname === '/delete_process'){
       var body = '';
