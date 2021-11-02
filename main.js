1ec3091871c31af213361d779e41584817479ebf
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-02 13:02:31 +0900

    모듈관련 주석

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+var http = require('http');//html페이지를 전달하는데 주로 쓰임
+//var fs = require('fs');//파일 처리와 관련된 모듈
+var url = require('url');//url정보를 객체로 가져와 분석하거나 url객체를 문자열로 바꿔주는 기능 수행
+var qs = require('querystring');//querystring 모듈은 url 객체의 query와 관련된 모듈. url 모듈의 두 번째 인자 값을 조정함으로써 해결할 수도 있다.
 var template = require('./lib/template.js');
-var path = require('path');
+var path = require('path');//파일/폴더/디렉터리 등의 경로를 편리하게 설정할 수 있는 기능을 제공.
 var sanitizeHtml = require('sanitize-html');
 var mysql=require('mysql');
 var db = mysql.createConnection({
