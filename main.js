b9c0528c1786687f93c9cbd5b752a6eb7ce4a6dc
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-03 15:26:01 +0900

    module.exports=> 모듈화 후 필요한 곳에서 require 사용하여 호출

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http');//html페이지를 전달하는데 주로 쓰임
 //var fs = require('fs');//파일 처리와 관련된 모듈
 var url = require('url');//url정보를 객체로 가져와 분석하거나 url객체를 문자열로 바꿔주는 기능 수행
 var qs = require('querystring');//querystring 모듈은 url 객체의 query와 관련된 모듈. url 모듈의 두 번째 인자 값을 조정함으로써 해결할 수도 있다.
-var template = require('./lib/template.js');
+var template = require('./lib/template.js');//template 모듈 호출
 var path = require('path');//파일/폴더/디렉터리 등의 경로를 편리하게 설정할 수 있는 기능을 제공.
 var sanitizeHtml = require('sanitize-html');
 var mysql=require('mysql');
@@ -58,7 +58,7 @@ var app = http.createServer(function(request,response){
               </form>`
           );
           response.writeHead(200);
-          response.end(html);
+          response.end(html);//html 코드 서버에서 실행시킴
           })
         });
       }
