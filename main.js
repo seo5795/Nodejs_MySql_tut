e470c08f1092f18f177bdc2620c98f2925cc4b65
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-11-05 20:14:46 +0900

    검색기능: 프론트 구성하기

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ var app = http.createServer(function(request,response){
       author.update_process(request,response);
     }else if(pathname === '/author/delete_process'){
       author.delete_process(request,response);
+    }else if(pathname === '/search_process'){
+      topic.search_process(request,response);
     }
      else {
       response.writeHead(404);
